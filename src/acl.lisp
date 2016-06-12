3b2c566906f4553a6f2cff5bf4b7b4d9d9e8dff9
Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
2016-06-12 10:03:31 +0200

    Updated readme.text

--- src/acl.lisp
+++ src/acl.lisp
@@ -1,110 +1,244 @@
-;;; **********************************************************************
-;;; Copyright (C) 2002 Heinrich Taube (taube@uiuc.edu) 
-;;; This program is free software; you can redistribute it and/or
-;;; modify it under the terms of the GNU General Public License
-;;; as published by the Free Software Foundation; either version 2
-;;; of the License, or (at your option) any later version.
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;; **********************************************************************
-
-;;; $Name$
-;;; $Revision: 77 $
-;;; $Date: 2003-12-18 14:17:45 +0100 (Thu, 18 Dec 2003) $
-
 (in-package :cm)
 
-;;;
-;;; CLOS twiddling
-;;;      
+(defparameter *incudine-default-input* nil)
 
-(import '(clos:slot-definition-initargs
-	  clos:slot-definition-initform
-	  clos:slot-definition-name
-	  clos:class-direct-subclasses
-	  clos:class-direct-superclasses
-	  clos:class-direct-slots
-	  clos:class-slots
-	  clos:generic-function-name
-	  ))
+(defparameter *incudine-default-output* nil)
 
-(defun finalize-class (class) (clos:finalize-inheritance class))
+(defparameter *incudine-default-latency* 5)
 
-(defmethod validate-class ((class t) (superclass t))
-  ;; this is a no-op except in OpenMCL 014
-  t)
+(defparameter *incudine-default-inbuf-size* 512)
 
-(defun slot-definition-reader (x) x nil)
+(defparameter *incudine-default-outbuf-size* 2048)
 
-(defmethod make-load-form (obj) (cl:make-load-form obj))
+(defparameter *incudine-default-filter* 0)
 
-;;;
-;;; misc stuff
-;;;
+(defparameter *incudine-default-mask* 0)
 
-(defun exit () (excl:exit))
 
-(defun quit () (exit))
+(progn
+ (defclass incudine-stream (rt-stream midi-stream-mixin)
+           ((input :initform *incudine-default-input* :initarg :input
+             :accessor incudine-input)
+            (output :initform *incudine-default-output* :initarg
+             :output :accessor incudine-output)
+            (latency :initform *incudine-default-latency* :initarg
+             :latency :accessor rt-stream-latency)
+            (inbufsize :initform *incudine-default-inbuf-size*
+             :initarg :inbuf-size :accessor incudine-inbuf-size)
+            (outbufsize :initform *incudine-default-outbuf-size*
+             :initarg :outbuf-size :accessor incudine-outbuf-size)
+            (receive-data :initform (list nil nil nil nil) :accessor
+             rt-stream-receive-data)
+            (receive-mode :initform :message :initarg :receive-mode
+             :accessor rt-stream-receive-mode)
+            (filter :initform *incudine-default-filter* :initarg
+             :filter :accessor incudine-filter)
+            (mask :initform *incudine-default-mask* :initarg
+             :channel-mask :accessor incudine-channel-mask)
+            (offset :initform 0 :initarg :offset :accessor
+             incudine-offset))
+           #+metaclasses  (:metaclass io-class))
+ (defparameter <incudine-stream> (find-class 'incudine-stream))
+ (finalize-class <incudine-stream>)
+ (setf (io-class-file-types <incudine-stream>) '("*.ic"))
+ (values))
 
-(defun object-address (x)
-  (excl::pointer-to-address x))
+(defmethod print-object ((obj incudine-stream) port)
+           (let ((name (object-name obj))
+                 (pids (event-stream-stream obj))
+                 (*print-case* ':downcase))
+             (setf name
+                     (if name
+                         (format nil "~a \"~a\""
+                                 (class-name (class-of obj)) name)
+                         (format nil "~a"
+                                 (class-name (class-of obj)))))
+             (if pids
+                 (if (car pids)
+                     (if (cadr pids)
+                         (format port "#<~a (in:~d out:~d)>" name
+                                 (car pids) (cadr pids))
+                         (format port "#<~a (in:~d)>" name
+                                 (car pids)))
+                     (if (cadr pids)
+                         (format port "#<~a (out:~d)>" name
+                                 (cadr pids))
+                         (format port "#<~a>" name)))
+                 (format port "#<~a>" name))))
 
-(defconstant directory-delimiter #\/)
+(defmethod open-io ((obj incudine-stream) dir &rest args)
+  (declare (ignore dir args))
+           (when (not (io-open obj)))
+           obj)
 
-;(defun cd (&optional dir)
-;  (if (null dir)
-;    (namestring (excl:current-directory))
-;    (progn
-;      (tpl::cd-command dir)
-;      ;(namestring (excl:current-directory))
-;      (values))))
+(defmethod close-io ((obj incudine-stream) &rest mode)
+  (declare (ignore obj mode))
+           (values))
 
-(defun pwd ()
-  (namestring (excl:current-directory)))
+(defmethod initialize-io ((obj incudine-stream))
+  (declare (ignore obj))
+  )
 
-(defun cd (&optional (dir (user-homedir-pathname )))
-  (tpl::cd-command (namestring (truename dir))))
+(defun incudine-open (&rest args)
+  (apply #'open-io "incudine-rts.ic" t args))
 
-(defun shell (cmd &rest args) 
-  (excl:shell (apply #'format nil cmd args)))
+(defun incudine-open? (&optional (port (find-object "incudine-rts.ic")))
+  (if port
+      (let ((io (io-open port)))
+        (if io
+            (if (car io) (if (cadr io) :inout :in)
+                (if (cadr io) :out nil))
+            nil))
+      nil))
 
-(defun make-cm-script (dir mempath initpath)
-  (let ((lisp (system:command-line-argument 0))
-	(script (namestring (merge-pathnames #+win32 "cm.bat"
-                                             #-win32 "cm" 
-                                             dir))))
-    (format t "~%; Saving CM startup script in ~S" script)
-    (with-open-file (f script :direction :output
-                       :if-does-not-exist :create
-                       :if-exists :supersede)
-      (format f 
-              #+win32 "~a -I ~a -L ~a~%"
-              #-win32 "~a -I ~a -L ~a~%"
-              lisp mempath initpath))))
+(defun incudine-close (&optional (port (find-object "incudine-rts.ic")))
+  (if (incudine-open? port)
+      (progn
+       (if (equal ':running (recv? port)) (recv-stop port))
+       (close-io port ':force))
+      port)
+  port)
 
-(defun cm-image-dir ()
-  (namestring
-   (make-pathname
-    :directory (pathname-directory excl::*image-file*))))
+(defun pm-message->midi-message (pmm)
+  (declare (ignore pmm))
+  ;; (let ((status (incudine:message.status pmm)))
+  ;;   (if (< status 240)
+  ;;       (if (logtest status 128)
+  ;;           (let ((stat (ash (logand status 240) -4))
+  ;;                 (dat2 (incudine:message.data2 pmm)))
+  ;;             (when (and (= stat +ml-note-on-opcode+) (= 0 dat2))
+  ;;               (setf stat +ml-note-off-opcode+)
+  ;;               (setf dat2 127))
+  ;;             (make-channel-message stat (logand status 15)
+  ;;              (incudine:message.data1 pmm) dat2))
+  ;;           (error "pm-message->midi-message: running status :("))
+  ;;       (if (= status 255)
+  ;;           (error "pm-message->midi-message: meta message??!")
+  ;;           (let ((type (logand status 15)))
+  ;;             (if (= type 0)
+  ;;                 (error "pm-message->midi-message: sysex :(")
+  ;;                 (make-system-message (logior (ash type 4) 15) 0
+  ;;                  (incudine:message.data1 pmm)
+  ;;                  (incudine:message.data2 pmm)))))))
+  )
 
-(defun env-var (var)
-  (sys:getenv var))
+(defun midi-message->pm-message (mm)
+  (declare (ignore mm))
+  ;; (incudine:message
+  ;;  (logior (ash (midimsg-upper-status mm) 4)
+  ;;          (midimsg-lower-status mm))
+  ;;  (channel-message-data1 mm) (channel-message-data2 mm))
+  )
 
-(defun save-cm (path &rest args)
-  (declare (ignore args))
-  ;; what a pain...
-  (dolist (c (class-subclasses (find-class 'container)))
-    (finalize-class c))
-  (let ((fn #'(lambda ()
-		(declare (special *cm-readtable*)
-                         (function load-cminit cm))
-                (tpl:setq-default *readtable* *cm-readtable*)
-                (tpl:setq-default *package* (find-package :cm))
-                (load-cminit)
-                (cm-logo)
-                )))
-    (setf excl::*read-init-files* t)
-    (push fn excl::*restart-actions*)
-    (excl:dumplisp :name path)))
+(defmethod midi-write-message
+           ((obj integer) (str incudine-stream) scoretime data)
+  (declare (ignore obj str scoretime data))
+  ;; (cond ((sysex-p obj))
+           ;;       ((or (channel-message-p obj) (system-message-p obj))
+           ;;        (incudine:writeshort (second (io-open str))
+           ;;                             (if
+           ;;                              (scheduling-mode? ':events)
+           ;;                              (+ (round (* scoretime 1000))
+           ;;                                 (incudine-offset str))
+           ;;                              (incudine:time))
+           ;;                             (midi-message->pm-message
+           ;;                              obj))))
+           )
+
+(defmethod write-event ((obj midi) (str incudine-stream) scoretime)
+  (declare (ignore obj str scoretime))
+  ;; (let ((keyn (midi-keynum obj))
+           ;;       (chan (midi-channel obj))
+           ;;       (ampl (midi-amplitude obj))
+           ;;       (sched (scheduling-mode)))
+           ;;   (ensure-velocity ampl keyn)
+           ;;   (ensure-microtuning keyn chan str)
+           ;;   (unless (< keyn 0)
+           ;;     (incudine:writeshort (second (io-open str))
+           ;;                          (if (eq sched ':events)
+           ;;                              (+ (round (* scoretime 1000))
+           ;;                                 (incudine-offset str))
+           ;;                              (incudine:time))
+           ;;                          (incudine:message
+           ;;                           (logior 144 (logand chan 15))
+           ;;                           (logand keyn 127)
+           ;;                           (logand ampl 127)))
+           ;;     (enqueue *qentry-message*
+           ;;      (make-note-off chan keyn 127)
+           ;;      (+ scoretime (midi-duration obj)) nil sched)
+           ;;     )
+           ;;   (values))
+           )
+
+(defmethod write-event
+    ((obj midi-event) (str incudine-stream) scoretime)
+  (break "write-event: ~a to ~a, time: ~a~%" obj str scoretime)
+  (format t "write-event: ~a to ~a, time: ~a~%" obj str scoretime)
+  ;; (midi-write-message (midi-event->midi-message obj) str
+           ;;  scoretime nil)
+           )
+
+(defmethod write-event
+           ((obj integer) (str incudine-stream) scoretime)
+  (declare (ignore obj str scoretime))
+  ;; (midi-write-message obj str scoretime nil)
+  )
+
+(defun output (msg &key at (to *out*) raw)
+  (declare (ignore msg at to raw))
+  ;; (cond
+  ;;  ((numberp msg)
+  ;;   (incudine:writeshort (second (io-open to))
+  ;;                        (or at (incudine:time))
+  ;;                        (if raw msg
+  ;;                            (midi-message->pm-message msg))))
+  ;;  ((stringp msg)
+  ;;   (incudine:writesysex (second (io-open to))
+  ;;                        (or at (incudine:time)) msg))
+  ;;  (t (incudine:write (second (io-open to)) msg raw)))
+  (values))
+
+#|
+(defun parse-pm-output (forms clauses ops)
+  clauses
+  ops
+  (let ((head forms)
+        (oper (pop forms))
+        (expr nil)
+        (args (list))
+        (to nil)
+        (loop '()))
+    (when (null forms)
+      (loop-error ops head "Missing '" oper "' expression."))
+    (setf expr (pop forms))
+    (do ((stop nil))
+        ((or stop (null forms)))
+      (case (car forms)
+        ((to :to)
+         (when (null (cdr forms))
+           (loop-error ops head "Missing '" oper " to' expression."))
+         (setf args (nconc args (list ':to (cadr forms))))
+         (setf to t)
+         (setf forms (cddr forms)))
+        ((at :at)
+         (when (null (cdr forms))
+           (loop-error ops head "Missing '" oper " at' expression."))
+         (setf args (nconc args (list ':at (cadr forms))))
+         (setf forms (cddr forms)))
+        ((raw :raw)
+         (when (null (cdr forms))
+           (loop-error ops head "Missing '" oper
+            " raw' expression."))
+         (setf args (nconc args (list ':raw (cadr forms))))
+         (setf forms (cddr forms)))
+        (t (setf stop t))))
+    (unless to (setf args (nconc args (list ':to '*out*))))
+    (setf loop (list `(,oper ,expr ,@args)))
+    (values (make-loop-clause 'operator oper 'looping loop) forms)))
+
+(defparameter *process-operators*
+  (append *process-operators*
+          (list
+           (list 'incudine:output #'parse-pm-output 'task 'to 'at
+                 'raw))))
+|#
